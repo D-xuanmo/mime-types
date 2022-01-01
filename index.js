eeb35a1f12ffb014d616d76ff2598a052f10b20f
D-xuanmo <me@xuanmo.xin>
2022-01-01 15:41:35 +0800

    feat: awebp type

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ export default function (filename = '') {
   try {
     let type = ''
     const { suffix } = /\.?(?<suffix>[\w]+)$/.exec(filename).groups
-    const length = Object.keys(mimeTypes).length
     for (const [_key, _value] of Object.entries(mimeTypes)) {
       const keys = _key.split('|')
       if (keys.includes(suffix.toLocaleLowerCase())) {
